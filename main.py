2056fa32ef70dc1336708525a0c00474d5ad4e79
Oguzhan Kandakoglu <oguzhankandakoglu@gmail.com>
2020-10-02 15:17:21 +0000

    Query post/get eklendi

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import encrypt as enc
 import aborty
 import os
 
+"""
+Yapılacaklar:
+    1-) Kuyruğa link eklerken bocuk listesinde olup olmadığını kontrol et.
+"""
+
+
 # Init app
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -16,7 +22,7 @@ db = SQLAlchemy(app)
 # Init
 ma = Marshmallow(app)
 
-# Bocuk Model
+####################################### Bocuk Model #######################################
 class Bocuk(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     token = db.Column(db.String, unique=True)
@@ -38,7 +44,6 @@ bocuks_schema = BocukSchema(many=True)
 # Create a Bocuk
 @app.route('/bocuk', methods=['POST'])
 def add_product():
-
     text =  request.json['text']
     token = enc.encrypt(text)
     result = Bocuk.query.filter_by(token=token).first()
@@ -64,6 +69,53 @@ def get_bocuk(token):
     
     return bocuk_schema.jsonify(bocuk)
     
+
+####################################### Query Model #######################################
+class Query(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    link = db.Column(db.String)
+    level = db.Column(db.String)
+
+    def __init__(self, link, level):
+        self.link = link
+        self.level = level
+
+# Query Schema
+class QuerySchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'link', 'level')
+
+# Init Schema
+query_schema = QuerySchema()
+queries_schema = QuerySchema(many=True)
+
+# Add link to Query
+@app.route('/query', methods=['POST'])
+def append_link():
+    token = request.json['token']
+    if not Bocuk.query.filter_by(token=token).first():
+        return aborty.abort(403, "You are not BOCUK!")
+    else:
+        link = request.json['link']
+        level = request.json['level']
+        new_link = Query(link, level)
+        db.session.add(new_link)
+        db.session.commit()
+        return query_schema.jsonify(new_link)
+
+# Get All Links
+@app.route('/query', methods=['GET'])
+def get_links():
+    query = Query.query.all()
+    result = queries_schema.dump(query)
+    return jsonify(result)
+
+####################################### TakenQuery Model #######################################
+
+
+
+####################################### CrawledQuery Model #######################################
+
 
 
 # Run Server
--- test.py
+++ test.py
@@ -2,7 +2,15 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-# response = requests.post(BASE + "bocuk", json={"text": "oz"})
-# response = requests.get(BASE + "bocuk/2dcb56dcf9ccfce02857f07a3c326745")
-response = requests.get(BASE + "bocuk")
+# response = requests.post(BASE + "bocuk", json={"text": "oz"}) # post test
+# response = requests.get(BASE + "bocuk/2dcb56dcf9ccfce02857f07a3c326745") # get bocuk by token
+# response = requests.get(BASE + "bocuk") # get bocuks
+
+# response = requests.post(BASE + "query", json={ # post linkj
+#                                                 "link": "bulurum.com",
+#                                                 "level": "4",
+#                                                 "token": "2dcb56dcf9ccfce02857f07a3c326745"
+#                                                 })
+response = requests.get(BASE + "query") # get query
+
 print(response.json())
